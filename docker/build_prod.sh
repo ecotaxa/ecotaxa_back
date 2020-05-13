6519c065f7384dda4fa5ece51fcc0ef452f55de3
grololo06 <laurent.salinas@laposte.net>
2020-05-13 18:32:18 +0200

    Docker adjustments after attempt on my PC.

--- docker/build_prod.sh
+++ docker/build_prod.sh
@@ -6,6 +6,8 @@ rsync -avr --delete --exclude-from=not_to_copy.lst ../py/ py/
 cp ../link.ini .
 docker build -t grololo06/ecotaxaback -f prod_image/Dockerfile .
 # once built:
-# docker push grololo06/ecotaxaback:latest
+#docker images
+#docker tag 933912b1ecd2 grololo06/ecotaxaback:2.3
+#docker push grololo06/ecotaxaback:2.3
 # needing before:
 # docker login
