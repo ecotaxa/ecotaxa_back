ddfa7c5f61194c220af2a462246a7dca9b884bc1
grololo06 <laurent.salinas@laposte.net>
2020-05-26 08:36:03 +0200

    Better HOWTO for docker.

--- docker/build_prod.sh
+++ docker/build_prod.sh
@@ -6,8 +6,7 @@ rsync -avr --delete --exclude-from=not_to_copy.lst ../py/ py/
 cp ../link.ini .
 docker build -t grololo06/ecotaxaback -f prod_image/Dockerfile .
 # once built:
-#docker images
-#docker tag 933912b1ecd2 grololo06/ecotaxaback:2.3
-#docker push grololo06/ecotaxaback:2.3
+#   docker tag grololo06/ecotaxaback:latest grololo06/ecotaxaback:2.3
+#   docker push grololo06/ecotaxaback:2.3
 # needing before:
-# docker login
+#   docker login
