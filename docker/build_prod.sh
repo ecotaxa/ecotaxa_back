6a0b8de17203858b5cc3bccc415b7b34405f64fd
grololo06 <laurent.salinas@laposte.net>
2020-09-06 19:15:42 +0200

    More resources for the docker.

--- docker/build_prod.sh
+++ docker/build_prod.sh
@@ -5,7 +5,7 @@
 rsync -avr --delete --exclude-from=not_to_copy.lst ../py/ py/
 cp ../link.ini .
 docker build -t grololo06/ecotaxaback -f prod_image/Dockerfile .
-# once built:
+# once built, replace 2.3 with the version...:
 #   docker tag grololo06/ecotaxaback:latest grololo06/ecotaxaback:2.3
 #   docker push grololo06/ecotaxaback:2.3
 # needing before:
--- docker/prod_image/start.sh
+++ docker/prod_image/start.sh
@@ -2,5 +2,5 @@
 set -e
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
-# 6 workers as each is blocking when not async
-exec gunicorn -w 6 --bind=0.0.0.0 --worker-class uvicorn.workers.UvicornWorker main:app
+# 16 workers as each is blocking when not async
+exec gunicorn -w 16 --threads 2 --bind=0.0.0.0 --worker-class uvicorn.workers.UvicornWorker main:app
