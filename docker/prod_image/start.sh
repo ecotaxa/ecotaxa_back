0e72c39d4f9507af91c2d5f1c6efacfdaf38eb85
grololo06 <laurent.salinas@laposte.net>
2020-10-14 06:10:46 +0200

    Align timeout with nginx.

--- docker/prod_image/start.sh
+++ docker/prod_image/start.sh
@@ -3,4 +3,4 @@ set -e
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 # 16 workers as each is blocking when not async
-exec gunicorn -w 16 --threads 2 --bind=0.0.0.0 --worker-class uvicorn.workers.UvicornWorker main:app
+exec gunicorn -w 16 --threads 2 --bind=0.0.0.0 --timeout 600 --worker-class uvicorn.workers.UvicornWorker main:app
