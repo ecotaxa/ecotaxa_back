1ff72411be0a59b96646a61688c63d6ba7826523
grololo06 <laurent.salinas@laposte.net>
2020-11-11 07:36:53 +0100

    Refresh docker script.

--- docker/build_prod.sh
+++ docker/build_prod.sh
@@ -6,7 +6,7 @@ rsync -avr --delete --exclude-from=not_to_copy.lst ../py/ py/
 cp ../link.ini .
 docker build -t grololo06/ecotaxaback -f prod_image/Dockerfile .
 # once built, replace 2.3 with the version...:
-#   docker tag grololo06/ecotaxaback:latest grololo06/ecotaxaback:2.3
-#   docker push grololo06/ecotaxaback:2.3
+#   docker tag grololo06/ecotaxaback:latest grololo06/ecotaxaback:2.4
+#   docker push grololo06/ecotaxaback:2.4
 # needing before:
 #   docker login
