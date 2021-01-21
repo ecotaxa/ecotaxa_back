907996bfe2d0f3e0a6df1b522cdc1c8e1bc84c29
grololo06 <laurent.salinas@laposte.net>
2021-01-21 11:48:56 +0100

    For ecotaxa/ecotaxa_dev#511: Better help text for licenses.

--- py/BO/DataLicense.py
+++ py/BO/DataLicense.py
@@ -43,16 +43,17 @@ class DataLicense(object):
                    LicenseEnum.NO_LICENSE: "?",
                    }
 
-    EXPLANATIONS = {LicenseEnum.CC0: "https://creativecommons.org/publicdomain/zero/1.0/",
-                    LicenseEnum.CC_BY: "https://creativecommons.org/licenses/by/4.0/",
-                    # LicenseEnum.CC_BY_SA: "https://creativecommons.org/licenses/by-sa/4.0/",
-                    LicenseEnum.CC_BY_NC: "https://creativecommons.org/licenses/by-nc/4.0/",
-                    # LicenseEnum.CC_BY_NC_SA: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
-                    # LicenseEnum.CC_BY_ND: "https://creativecommons.org/licenses/by-nd/4.0/",
-                    # LicenseEnum.CC_BY_NC_ND: "https://creativecommons.org/licenses/by-nc-nd/4.0/",
-                    LicenseEnum.Copyright: "All rights reserved",
-                    LicenseEnum.NO_LICENSE: "Not chosen",
-                    }
+    EXPLANATIONS = {
+        LicenseEnum.CC0: r'<a href="https://creativecommons.org/publicdomain/zero/1.0/" rel="nofollow"><strong>CC-0</strong></a>: all registered EcoTaxa users are free to download, redistribute, modify, and build upon the data, with no conditions. Other databases can index the data. The data falls into the worldwide public domain. This is the license preferred by <a href="https://obis.org/manual/policy/" rel="nofollow">OBIS</a> and <a href="https://www.gbif.org/terms" rel="nofollow">GBIF</a>.',
+        LicenseEnum.CC_BY: r'<a href="https://creativecommons.org/licenses/by/4.0/" rel="nofollow"><strong>CC-BY</strong></a>: all registered EcoTaxa users are free to download, redistribute, modify, and build upon the data, as long as they cite the dataset and its authors. Other databases can index the data.',
+        # LicenseEnum.CC_BY_SA: "https://creativecommons.org/licenses/by-sa/4.0/",
+        LicenseEnum.CC_BY_NC: r'<a href="https://creativecommons.org/licenses/by-nc/4.0/" rel="nofollow"><strong>CC-BY-NC</strong></a>: all registered EcoTaxa users are free to download, redistribute, modify, and build upon the data, as long as they cite the dataset and its authors, and do not use it for commercial purpose ("primarily intended for or directed toward commercial advantage or monetary compensation"). Other databases can index the data.',
+        # LicenseEnum.CC_BY_NC_SA: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+        # LicenseEnum.CC_BY_ND: "https://creativecommons.org/licenses/by-nd/4.0/",
+        # LicenseEnum.CC_BY_NC_ND: "https://creativecommons.org/licenses/by-nc-nd/4.0/",
+        LicenseEnum.Copyright: r'<strong>Copyright</strong>: only contributors to this project have rights on this data. This prevents its distribution in any kind of database.',
+        LicenseEnum.NO_LICENSE: "Not chosen",
+        }
 
     RESTRICTION = {LicenseEnum.CC0: 0,
                    LicenseEnum.CC_BY: 1,
