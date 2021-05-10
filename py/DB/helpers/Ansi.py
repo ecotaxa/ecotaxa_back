f841a5a10856609f5bc2c1c4202a8dfba381abeb
grololo06 <laurent.salinas@laposte.net>
2021-05-10 18:40:41 +0200

    Bump python to 3.8 and many packages to latest - forgotten part of file of course.

--- py/DB/helpers/Ansi.py
+++ py/DB/helpers/Ansi.py
@@ -2,3 +2,5 @@
 # This file is part of Ecotaxa, see license.md in the application root directory for license informations.
 # Copyright (C) 2015-2020  Picheral, Colin, Irisson (UPMC-CNRS)
 #
+# noinspection PyUnresolvedReferences
+from sqlalchemy.sql.functions import current_timestamp
