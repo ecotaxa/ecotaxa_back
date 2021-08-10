ea02768dc3a054f7afa5e6a6d2c3eb9b46422f71
grololo06 <laurent.salinas@laposte.net>
2021-08-10 17:03:25 +0200

    ecotaxa/ecotaxa_dev#606: Cron AKA nightly job rewrite in back-end. Minimal test.

--- QA/py/tests/test_admin.py
+++ QA/py/tests/test_admin.py
@@ -3,8 +3,10 @@ import logging
 from starlette import status
 
 from tests.credentials import ADMIN_AUTH, USER_AUTH
+from tests.test_jobs import wait_for_stable, check_job_ok
 
 PROJECT_DIGEST_URL = "/admin/images/{project_id}/digest?max_digests=100"
+NIGHTLY_URL = "/admin/nightly"
 
 
 def test_admin_images(config, database, fastapi, caplog):
@@ -29,3 +31,22 @@ def test_admin_images(config, database, fastapi, caplog):
     rsp = fastapi.get(url, headers=ADMIN_AUTH)
     assert rsp.status_code == status.HTTP_200_OK
     assert rsp.json() == "Digest for 0 images done."
+
+
+def test_nightly_job(config, database, fastapi, caplog):
+    # TODO: Not a real test, as we can't know in advance when the test runs, so the output
+    # can't be verified against a reference.
+
+    # Simple user cannot
+    rsp = fastapi.get(NIGHTLY_URL, headers=USER_AUTH)
+    assert rsp.status_code == status.HTTP_403_FORBIDDEN
+
+    # Only Admin can
+    caplog.set_level(logging.DEBUG)
+    rsp = fastapi.get(NIGHTLY_URL, headers=ADMIN_AUTH)
+    assert rsp.status_code == status.HTTP_200_OK
+
+    job_id = rsp.json()
+    job = wait_for_stable(job_id)
+    check_job_ok(job)
+
