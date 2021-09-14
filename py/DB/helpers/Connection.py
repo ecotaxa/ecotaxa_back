95b6606aae9ae0a53d8a96b6241ecd11430214be
grololo06 <laurent.salinas@laposte.net>
2021-09-14 13:38:16 +0200

    Bump DB driver & sqlalchemy versions, take the occasion to have a real read-only connection, hopefully optimized by PG.

--- py/DB/helpers/Connection.py
+++ py/DB/helpers/Connection.py
@@ -6,7 +6,6 @@
 import sqlalchemy
 from sqlalchemy import MetaData
 from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy.pool import QueuePool
 
 from helpers.DynamicLogs import get_logger
 
@@ -16,7 +15,7 @@ logger = get_logger(__name__)
 def check_sqlalchemy_version():
     # noinspection PyUnresolvedReferences
     version = sqlalchemy.__version__
-    expected_version = "1.4.14"
+    expected_version = "1.4.23"
     if version != expected_version:  # pragma: no cover
         logger.fatal("Not the expected SQLAlchemy version (%s instead of %s), exiting to avoid data corruption",
                      version, expected_version)
@@ -36,13 +35,6 @@ def check_sqlalchemy_version():
 #     crs.close()
 #     dbapi_conn.commit()
 
-
-class ReadOnlyQueuePool(QueuePool):
-    def __init__(self, *args, **kwargs):
-        kwargs['reset_on_return'] = None
-        super().__init__(*args, **kwargs)
-
-
 class Connection(object):
     """
         A connection to the DB via SQLAlchemy.
@@ -53,10 +45,9 @@ class Connection(object):
             Open a SQLAlchemy connection, i.e. an engine.
         """
         if read_only:
-            pool_class = ReadOnlyQueuePool
+            exec_options = {'postgresql_readonly': True}
         else:
-            # Doc says: default for all dialects except SQLite
-            pool_class = QueuePool
+            exec_options = {}
         # We connect with the help of the PostgreSQL URL
         url = 'postgresql://{}:{}@{}:{}/{}'
         url = url.format(user, password, host, port, db)
@@ -64,12 +55,12 @@ class Connection(object):
                                           echo=False, echo_pool=False,
                                           # echo=True, echo_pool="debug",
                                           executemany_mode='batch',
-                                          poolclass=pool_class,
                                           # Not needed anyway it's not async anywhere and default is 5
                                           # pool_size=20, max_overflow=5,
                                           # This way we can restart the DB and sessions will re-establish themselves
                                           # the cost is 1 (simple) query per connection pool recycle.
                                           pool_pre_ping=True,
+                                          execution_options=exec_options,
                                           connect_args={"application_name": "ecotaxa_back"})
         self.session_factory = sessionmaker(bind=engine)
         self._meta: MetaData = sqlalchemy.MetaData(bind=engine)
