82556ff171f2ac9f4cf7aee40134fa86ff4f229c
grololo06 <laurent.salinas@laposte.net>
2021-10-04 19:15:36 +0200

    ecotaxa/ecotaxa_dev#605: Useful code in case the GPU is on strike.

--- py/ML/helpers/tensorflow_cfg.py
+++ py/ML/helpers/tensorflow_cfg.py
@@ -11,12 +11,20 @@ import os
 # NB: does not work when run on the command line apparently...
 
 # store models downloaded from TFHub in the user's home to avoid permission problems
+import sys
+
 os.environ['TFHUB_CACHE_DIR'] = os.path.expanduser('~/.tfhub_modules/')
 
 # Set a memory limit on tensorflow, which otherwise takes all the GPU memory by default
 # which would be fine except that other GPU-related modules (CUBLAS) have 0 byte left for them.
 import tensorflow as configured_tf
 gpus = configured_tf.config.experimental.list_physical_devices('GPU')
+
+if len(gpus) == 0:
+    print(""" NO GPU FOUND. If it worked before, you can try:
+root:~# rmmod nvidia_uvm
+root:~# nvidia-modprobe 
+    """, file=sys.stderr)
 
 # # either allow memory to grow as needed (less efficient -- and seems broken)
 # tf.config.experimental.set_memory_growth(gpus[0], True)
