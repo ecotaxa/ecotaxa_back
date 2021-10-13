7a14c593e5c575c3a1cd1af7591f17ba4d4f310c
grololo06 <laurent.salinas@laposte.net>
2021-10-13 06:08:14 +0200

    ecotaxa/ecotaxa_dev#605: RF classes.

---
+++ py/ML/Random_forest_trainer.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# This file is part of Ecotaxa, see license.md in the application root directory for license informations.
+# (c) 2021 Jean-Olivier Irisson, GNU General Public License v3
+#
+
+#
+# @see README.md
+#
+
+class RandomForestTrainer(object):
+    """
+
+    """
+    def __init__(self):
+        pass
