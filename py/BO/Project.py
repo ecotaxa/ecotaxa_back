92bd6474831b7f145c385bad3d636af551ab34f9
grololo06 <laurent.salinas@laposte.net>
2023-03-04 10:02:53 +0100

    ecotaxa/ecotaxa_front#843: Forgot to pre-load this child table.

--- py/BO/Project.py
+++ py/BO/Project.py
@@ -773,6 +773,7 @@ class ProjectBOSet(object):
         # qry = session.query(Project)
         qry = qry.options(subqueryload(Project.privs_for_members))
         qry = qry.options(subqueryload(Project.members))
+        qry = qry.options(subqueryload(Project.variables))
         qry = qry.filter(Project.projid == any_(prj_ids))
         self.projects: List[ProjectBO] = []
         # De-duplicate
