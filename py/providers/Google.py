e0c97941938b780ef154ff6672cc3ea37a42cf9b
bea777515 <beatrice.caraveo@imev-mer.fr>
2023-07-31 16:47:40 +0200

    verify_captcha function added

--- py/providers/Google.py
+++ py/providers/Google.py
@@ -5,7 +5,9 @@
 # Clients to Google API services, so far just one.
 #
 from typing import Optional
-
+from starlette.status import (
+    HTTP_422_UNPROCESSABLE_ENTITY,
+)
 import requests
 
 
@@ -33,3 +35,26 @@ class ReCAPTCHAClient(object):
         if rsp.status_code != 200 or not rsp.json()["success"]:
             return rsp.text.replace("\n", "")
         return None
+
+    def verify_captcha(self, no_bot: list) -> None:
+        if no_bot is None:
+            raise HTTPEception(
+                status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+                detail="reCaptcha verif needs data",
+            )
+        if len(no_bot) != 2:
+            raise HTTPEception(
+                status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+                detail="invalid no_bot reason 1",
+            )
+        for a_str in no_bot:
+            if len(a_str) >= 1024:
+                raise HTTPEception(
+                    status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+                    detail="invalid no_bot reason 2",
+                )
+        remote_ip = no_bot[0]
+        response = no_bot[1]
+        error = self.validate(remote_ip, response)
+        if error is None:
+            raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail=error)
