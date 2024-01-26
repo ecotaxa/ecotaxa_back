ff780aa14b4130c9f30d6b6ebd023a4b1ce8202c
emmaamblard <47526817+emmaamblard@users.noreply.github.com>
2024-01-26 09:32:33 +0000

    add similarity scores + test similarity search with filters on taxo

--- QA/py/tests/test_similarity_search.py
+++ QA/py/tests/test_similarity_search.py
@@ -8,6 +8,7 @@ from starlette import status
 from tests.credentials import ADMIN_AUTH
 from tests.jobs import get_job_and_wait_until_ok, api_check_job_ok
 from tests.test_objectset_query import _prj_query
+from tests.test_classification import classify_all
 
 from BO.Prediction import DeepFeatures
 
@@ -15,6 +16,13 @@ from API_operations.CRUD.ObjectParents import SamplesService
 
 OBJECT_SET_SIMILARITY_SEARCH_URL = "/object_set/similarity_search"
 
+copepod_id = 25828
+entomobryomorpha_id = 25835
+crustacea = 12846
+
+
+def similarity_scores(target_id, distances_to_target):
+    return [round(1 - d / max(distances_to_target), 4) for d in distances_to_target]
 
 def test_similarity_search(database, fastapi, caplog):
     caplog.set_level(logging.ERROR)
@@ -24,27 +32,32 @@ def test_similarity_search(database, fastapi, caplog):
 
     obj_ids = _prj_query(fastapi, ADMIN_AUTH, prj_id)
     assert len(obj_ids) == 8
+
+    # Prepare dummy features
+    features = list()
+    for i, oi in enumerate(obj_ids):
+        features.append([(i + 1) * 0.1] * 50)
+    features_df = pd.DataFrame(features, index=obj_ids)
+
+    target_id = obj_ids[0]
+    distances_to_target = [np.linalg.norm(features_df.loc[target_id] - features_df.loc[oi]) for oi in obj_ids]
     
     # Test similarity search without features
     url = OBJECT_SET_SIMILARITY_SEARCH_URL
     req = {
         "project_id": prj_id,
-        "target_id": obj_ids[0],
+        "target_id": target_id,
     }
     filters = {}
     req_and_filters = {"filters": filters, "request": req}
     rsp = fastapi.post(url, headers=ADMIN_AUTH, json=req_and_filters)
 
     assert rsp.status_code == status.HTTP_200_OK
-    assert rsp.json()["neighbor_ids"] == []
     assert rsp.json()["message"] == "Missing CNN features, feature extraction job launched"
+    assert rsp.json()["neighbor_ids"] == []
+    assert rsp.json()["sim_scores"] == []
 
     # Insert dummy features
-    features = list()
-    for i, oi in enumerate(obj_ids):
-        features.append([(i + 1) * 0.1] * 50)
-    features_df = pd.DataFrame(features, index=obj_ids)
-
     with SamplesService() as sce:
         n_inserts = DeepFeatures.save(sce.session, features_df)
         assert n_inserts == 8
@@ -54,12 +67,34 @@ def test_similarity_search(database, fastapi, caplog):
     url = OBJECT_SET_SIMILARITY_SEARCH_URL
     req = {
         "project_id": prj_id,
-        "target_id": obj_ids[0],
+        "target_id": target_id,
     }
     filters = {}
     req_and_filters = {"filters": filters, "request": req}
     rsp = fastapi.post(url, headers=ADMIN_AUTH, json=req_and_filters)
 
     assert rsp.status_code == status.HTTP_200_OK
-    assert rsp.json()["neighbor_ids"] == obj_ids
     assert rsp.json()["message"] == "Success"
+    assert rsp.json()["neighbor_ids"] == obj_ids
+    assert rsp.json()["sim_scores"] == similarity_scores(target_id, distances_to_target)
+
+    # Set different taxo ids
+    classify_all(fastapi, obj_ids[0:3], copepod_id)
+    classify_all(fastapi, obj_ids[3:5], entomobryomorpha_id)
+    classify_all(fastapi, obj_ids[5:8], crustacea)
+    taxo_ids_to_filter = [copepod_id, entomobryomorpha_id]
+
+    # Test similarity search with filters on taxo
+    url = OBJECT_SET_SIMILARITY_SEARCH_URL
+    req = {
+        "project_id": prj_id,
+        "target_id": target_id,
+    }
+    filters = {"taxo": ",".join([str(taxo_id) for taxo_id in taxo_ids_to_filter])}
+    req_and_filters = {"filters": filters, "request": req}
+    rsp = fastapi.post(url, headers=ADMIN_AUTH, json=req_and_filters)
+
+    assert rsp.status_code == status.HTTP_200_OK
+    assert rsp.json()["message"] == "Success"
+    assert rsp.json()["neighbor_ids"] == obj_ids[0:5]
+    assert rsp.json()["sim_scores"] == similarity_scores(target_id, distances_to_target[0:5])
