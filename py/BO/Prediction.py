980175fb1ed63548cb8ab9c72edde537eaabd95a
Victor Reutenauer <inscription@moilerat.com>
2024-02-17 14:13:20 +0100

    mention emma and lovnower for ss and cnnvector in license (only comment change) https://app.clickup.com/t/86bwqp6jp

--- py/BO/Prediction.py
+++ py/BO/Prediction.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # This file is part of Ecotaxa, see license.md in the application root directory for license informations.
-# Copyright (C) 2015-2021  Picheral, Colin, Irisson (UPMC-CNRS)
+# Copyright (C) 2015-2024  Picheral, Colin, Irisson (UPMC-CNRS), Amblard (LOVNOWER)
 #
 
 #
