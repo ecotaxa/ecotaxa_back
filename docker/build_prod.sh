8d608a7ed7a6c3fc6773afc45ed1bf28948e3097
grololo06 <laurent.salinas@laposte.net>
2024-09-19 09:26:25 +0200

    chore: manual partial merge from master

--- docker/build_prod.sh
+++ docker/build_prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=2.7.0
+VERSION=2.7.6.3
 # In case of doubt on the image sanity or if you have time, uncomment below
 #NO_CACHE=--no-cache
 # Preliminary, log using ecotaxa docker account
