fcc3393cd01a0de3ade4b85717f6e45b57446311
grololo06 <laurent.salinas@laposte.net>
2024-11-04 11:00:11 +0100

    bugfix(memory): leak when raising a static exception for FastApi - forgotten primitive

--- py/helpers/fastApiUtils.py
+++ py/helpers/fastApiUtils.py
@@ -201,9 +201,9 @@ def _forbidden_exception():
         status_code=status.HTTP_403_FORBIDDEN, detail="You can't do this."
     )
 
-_not_found_exception = HTTPException(
-    status_code=status.HTTP_404_NOT_FOUND, detail="Not found."
-)
+
+def _not_found_exception():
+    return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found.")
 
 
 class RightsThrower(AbstractContextManager):
