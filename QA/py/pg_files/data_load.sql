b9913fb0f118b1c8fa429b1fdbab16b90eb453d1
bea777515 <beatrice.caraveo@imev-mer.fr>
2025-01-22 17:57:17 +0100

    add type to initial users

--- QA/py/pg_files/data_load.sql
+++ QA/py/pg_files/data_load.sql
@@ -11,14 +11,14 @@ CREATE ROLE readerole WITH
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO readerole;
 delete from users where id > 1;
 
-COPY public.users (id, email, password, name, organisation, status, preferences, country, usercreationdate, usercreationreason) FROM stdin;
-2	user	zero6	Ordinary User	Homework	1	{}	\N	2020-05-13 08:59:48.70106	\N
-3	creator	$6$rounds=656000$iPCU2dUnIQjcUnuW$JxMguozUT9nTun/N19yx.ugzTlUL4bZTRB9eOqECLx83kNSarP6uAMze1qu1ULjJ/OugQAMyAYxFI4EHy0yn9/	User Creating Projects	\N	1	{}	\N	2020-05-13 08:59:48.70106	\N
-4	user2	$6$	Ordinary User 2	\N	1	{}	\N	2020-09-27 06:39:48.70106	\N
-5	old_admin	nimda_dlo	Application Administrator Now Retired	\N	0	{"1": {"sortby": "", "ts": 1589266843.5535243, "sortorder": "asc", "dispfield": "", "statusfilter": "", "ipp": "100", "zoom": "100", "magenabled": "0", "popupenabled": "0"}, "2": {"sortby": "", "ts": 1589267270.5538993, "sortorder": "asc", "dispfield": "", "statusfilter": "", "ipp": "100", "zoom": "100", "magenabled": "0", "popupenabled": "0"}}	\N	2020-05-12 08:59:48.70106	\N
-6	real@users.com	fake_pwd	Real User	Institut de la Mer de Villefranche - IMEV	0	{"1": {"sortby": "", "ts": 1589266843.5535243, "sortorder": "asc", "dispfield": "", "statusfilter": "", "ipp": "100", "zoom": "100", "magenabled": "0", "popupenabled": "0"}}	France	2020-10-26 08:59:48.70106	\N
-7	ucrank4@gllmmlgdfggloupgov.uk	fake_pwd	Udale Crank	Institut de la Mer de Villefranche - IMEV	1	{"1": {"sortby": "", "ts": 1589266843.5535243, "sortorder": "asc", "dispfield": "", "statusfilter": "", "ipp": "100", "zoom": "100", "magenabled": "0", "popupenabled": "0"}}	UK	2020-10-26 08:59:48.70106	\N
-8	real2@users.com	fake_pwd	Real User 3	Double Dash - Institute - DDORG	0	{}	Chile	2023-01-01 08:59:48.70106	\N
+COPY public.users (id, type, email, password, name, organisation, status, preferences, country, usercreationdate, usercreationreason) FROM stdin;
+2 users	user	zero6	Ordinary User	Homework	1	{}	\N	2020-05-13 08:59:48.70106	\N
+3	users creator	$6$rounds=656000$iPCU2dUnIQjcUnuW$JxMguozUT9nTun/N19yx.ugzTlUL4bZTRB9eOqECLx83kNSarP6uAMze1qu1ULjJ/OugQAMyAYxFI4EHy0yn9/	User Creating Projects	\N	1	{}	\N	2020-05-13 08:59:48.70106	\N
+4	users user2	$6$	Ordinary User 2	Homework	1	{}	\N	2020-09-27 06:39:48.70106	\N
+5	users old_admin	nimda_dlo	Application Administrator Now Retired	\N	0	{"1": {"sortby": "", "ts": 1589266843.5535243, "sortorder": "asc", "dispfield": "", "statusfilter": "", "ipp": "100", "zoom": "100", "magenabled": "0", "popupenabled": "0"}, "2": {"sortby": "", "ts": 1589267270.5538993, "sortorder": "asc", "dispfield": "", "statusfilter": "", "ipp": "100", "zoom": "100", "magenabled": "0", "popupenabled": "0"}}	\N	2020-05-12 08:59:48.70106	\N
+6	users real@users.com	fake_pwd	Real User	Institut de la Mer de Villefranche - IMEV	0	{"1": {"sortby": "", "ts": 1589266843.5535243, "sortorder": "asc", "dispfield": "", "statusfilter": "", "ipp": "100", "zoom": "100", "magenabled": "0", "popupenabled": "0"}}	France	2020-10-26 08:59:48.70106	\N
+7	users ucrank4@gllmmlgdfggloupgov.uk	fake_pwd	Udale Crank	Institut de la Mer de Villefranche - IMEV	1	{"1": {"sortby": "", "ts": 1589266843.5535243, "sortorder": "asc", "dispfield": "", "statusfilter": "", "ipp": "100", "zoom": "100", "magenabled": "0", "popupenabled": "0"}}	UK	2020-10-26 08:59:48.70106	\N
+8	users real2@users.com	fake_pwd	Real User 3	Double Dash - Institute - DDORG	0	{}	Chile	2023-01-01 08:59:48.70106	\N
 \.
 
 SELECT setval('seq_users', (SELECT max(id) FROM public.users), true);
@@ -27,6 +27,7 @@ COPY public.users_roles (user_id, role_id) FROM stdin;
 7	2
 \.
 
+INSERT INTO organizations (name) SELECT DISTINCT organisation FROM users WHERE organisation IS NOT NULL;
 -- The used ones are in first
 -- e.g. to get some data with lineage:
 -- ecotaxa4=# copy (select * from taxonomy where id in (92731, 85117, 56693, 25928, 16656, 12861,11513,2367,382,8)) to '/tmp/cp.sql';
