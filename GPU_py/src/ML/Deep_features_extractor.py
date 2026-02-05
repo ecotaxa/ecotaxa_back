0e11e7afac207f72344a29fc8d7a5dd5cccb635a
grololo06 <laurent.salinas@laposte.net>
2026-02-05 17:24:35 +0100

    dev(GPU): attempt to go to higher python

--- GPU_py/src/ML/Deep_features_extractor.py
+++ GPU_py/src/ML/Deep_features_extractor.py
@@ -1,1 +1,167 @@
-../../../py/ML/Deep_features_extractor.py
+# -*- coding: utf-8 -*-
+# This file is part of Ecotaxa, see license.md in the application root directory for license informations.
+# Copyright (C) 2015-2021  Picheral, Colin, Irisson (UPMC-CNRS)
+#
+
+#
+# @see README.md
+#
+# This is step 3 of the whole process but the first step in EcoTaxa
+#
+# 50 CNN features are extracted from the images and stored into obj_cnn_features DB table.
+#
+
+import pickle
+from io import StringIO
+from typing import IO, Dict
+
+import pandas as pd  # type: ignore
+import tf_keras as keras
+from sklearn.decomposition import PCA
+
+from DB.Object import ObjectIDT
+from FS.MachineLearningModels import SavedModels
+from FS.Vault import Vault
+from helpers.DynamicLogs import get_logger
+from .Base_ML import MachineLearningBase
+from .helpers import generator  # type: ignore # custom data generator
+# Import the library, only after having tweaked it
+from .helpers.tensorflow_cfg import configured_tf  # type: ignore
+
+logger = get_logger(__name__)
+
+
+class DeepFeaturesExtractor(MachineLearningBase):
+    """
+    Extract CNN features from a set of objects for which renew is necessary.
+    """
+
+    BATCH_SIZE = 16  # size of images batches in GPU memory
+    WORKERS = 10  # number of parallel threads to prepare batches
+
+    def __init__(self, vault: Vault, model_dir: SavedModels):
+        """
+        :param vault: the vault for finding images
+        :param model_dir: directory to read stored models
+        """
+        super().__init__(vault, model_dir)
+
+    def run(self, ids_and_images: Dict[ObjectIDT, str], model_name: str):
+        logger.info("Load feature extractor and dimensionality reducer")
+
+        # Note: sklearn objects are unpickled there, so it's needed as a dep
+        input_shape, my_fe, pca = self.load_model(model_name)
+        crop = self.read_crop(model_name)
+
+        logger.info("Load data")
+
+        # Prepare a df with input data
+        df_data = [(obj, fil, None) for obj, fil in ids_and_images.items()]
+        df = pd.DataFrame(df_data, columns=["id", "img_path", "label"])
+        df.set_index(["id"], inplace=True, verify_integrity=True)
+
+        logger.info("Extract features")
+
+        features_df = self.predict_dataframe(df, input_shape, crop, my_fe, pca)
+
+        return features_df
+
+    def predict_dataframe(self, in_df, input_shape, crop, my_fe, pca):
+        """
+        Predict what's in in_df and return the result dataframe.
+        """
+        # prepare data batches
+        batches = generator.EcoTaxaGenerator(
+            images_paths=self.full_img_paths(in_df.img_path.values),
+            input_shape=input_shape,
+            labels=None,
+            classes=None,
+            # NB: we don't need the labels here, we just run images through the network
+            batch_size=self.BATCH_SIZE,
+            augment=False,
+            shuffle=False,
+            crop=crop,
+        )
+        # extract features by going through the batches
+        full_features = my_fe.predict(
+            batches, max_queue_size=max(10, self.WORKERS * 2), workers=self.WORKERS
+        )
+        # and reduce their dimension
+        reduced_features = pca.transform(full_features)
+        # make a result dataframe
+        reduced_features_df = pd.DataFrame(reduced_features, index=in_df.index)
+        return reduced_features_df
+
+    def load_model(self, model_name):
+        """
+        Load saved model and PCA params, for the given model.
+        """
+        my_fe = keras.models.load_model(self.model_dir.extractor_path(model_name))
+        # get model input shape
+        input_shape = my_fe.layers[0].input_shape
+        # remove the None element at the start (which is where the batch size goes)
+        input_shape = tuple(x for x in input_shape if x is not None)
+        filepath = self.model_dir.reducer_pickle_path(model_name)
+        legacy_data = extract_data(filepath)
+        clean_pca = rebuild_pca(legacy_data)
+
+        return input_shape, my_fe, clean_pca
+
+    def test(self, csv_in: IO, model_name: str) -> StringIO:
+        """
+        Try the model.
+        """
+        logger.info("TEST: Load feature extractor and dimensionality reducer")
+
+        input_shape, my_fe, pca = self.load_model(model_name)
+
+        logger.info("TEST: Load data and extract features")
+
+        # read DataFrame with image ids, paths and labels
+        # NB: those would be in the database in EcoTaxa
+        df = pd.read_csv(csv_in, index_col="id")
+
+        no_crop = (0, 0, 0, 0)
+        reduced_features_df = self.predict_dataframe(
+            df, input_shape, no_crop, my_fe, pca
+        )
+
+        logger.info("TEST: Dumping a few rows")
+
+        ret = StringIO()
+        reduced_features_df.to_csv(ret)
+
+        return ret
+
+
+def extract_data(filepath):
+    class InsightUnpickler(pickle.Unpickler):
+        def find_class(self, module, name):
+            if "sklearn" in module:
+                return type(name, (object,), {})
+            return super().find_class(module, name)
+
+    with open(filepath, "rb") as f:
+        return InsightUnpickler(f).load()
+
+
+# 2. Reconstruction
+def rebuild_pca(legacy_obj):
+    # On crée une instance PCA de la version actuelle
+    modern_pca = PCA(
+        n_components=legacy_obj.n_components,
+        whiten=legacy_obj.whiten,
+        svd_solver=legacy_obj.svd_solver,
+    )
+
+    # On injecte manuellement tous les attributs "Fixed" que l'on a vus dans le log
+    modern_pca.components_ = legacy_obj.components_
+    modern_pca.mean_ = legacy_obj.mean_
+    modern_pca.explained_variance_ = legacy_obj.explained_variance_
+    modern_pca.explained_variance_ratio_ = legacy_obj.explained_variance_ratio_
+    modern_pca.singular_values_ = legacy_obj.singular_values_
+    modern_pca.n_components_ = legacy_obj.n_components_
+    modern_pca.n_features_in_ = legacy_obj.n_features_in_
+    modern_pca.noise_variance_ = legacy_obj.noise_variance_
+
+    return modern_pca
--- GPU_py/src/ML/helpers/generator.py
+++ GPU_py/src/ML/helpers/generator.py
@@ -1,1 +1,240 @@
-../../../../py/ML/helpers/generator.py
+#
+# Data generator for EcoTaxa images
+#
+# (c) 2021 Thelma Panaiotis, Jean-Olivier Irisson, GNU General Public License v3
+
+#
+# TODO: Implement crop for scale bar from https://github.com/ThelmaPana/plankton_classif_benchmark/blob/main/datasets.py
+#
+
+# import lycon  # image loading, manipulation
+import sys
+
+import cv2 as cv
+import numpy as np
+from sklearn.preprocessing import MultiLabelBinarizer  # one-hot encoder
+# from tensorflow.keras import utils
+from tf_keras import utils
+
+
+class EcoTaxaGenerator(utils.Sequence):
+    """
+    Generates data batches
+
+    Args:
+        images_paths (list/ndarray, str): paths to images
+        input_shape (tuple, int): dimensions of the input images for the network
+        labels (list/ndarray, str): image labels (as text) or None for an unlabelled batch
+        classes (list, str): all classes to consider for the classification,
+            or None for an unlabelled batch
+        batch_size (int): number of images in a batch
+            larger means faster/better training/evaluation but higher GPU memory use
+        shuffle (bool) : whether to shuffle inputs between epochs
+            usually True for training, False otherwise
+        augment (bool) : whether to use data augmentation on the input
+            usually True for training, False otherwise
+        upscale (bool) : whether to scale small images up to the input shape
+            if False, small images are just padded
+        crop (tuple, int) : number of pixels to crop from the top,right,bottom,left.
+
+    Returns:
+        A batch of `batch_size` images (4D ndarray) and one-hot encoded labels (2D ndarray)
+
+    """
+
+    def __init__(
+        self,
+        images_paths,
+        input_shape,
+        labels=None,
+        classes=None,
+        batch_size=32,
+        shuffle=False,
+        augment=False,
+        upscale=True,
+        crop=(0, 0, 0, 0),
+    ):
+        """Initialization of settings"""
+        # initialize constants
+        self.images_paths = images_paths
+        self.labels = labels
+        self.input_shape = input_shape
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.augment = augment
+        self.upscale = upscale
+        self.crop = crop
+
+        if labels is not None:
+            # initialise the one-hot encoder
+            mlb = MultiLabelBinarizer(classes=classes)
+            self.class_encoder = mlb
+
+        # print('Nb of images : ' + str(len(self.images_paths)))
+        # print('Nb of labels : ' + str(len(self.labels)))
+        # print('Input shape : ' + str(self.input_shape))
+        # print('Batch size : ' + str(self.batch_size))
+        # print('Shuffle inputs : ' + str(self.shuffle))
+        # print('Augment inputs : ' + str(self.augment))
+        # print('Upscale small images : ' + str(self.upscale))
+
+        self.on_epoch_end()
+
+    def __len__(self):
+        """Compute the number of batches to cover the dataset in one epoch"""
+        return int(np.ceil(len(self.images_paths) / self.batch_size))
+        # NB: use np.ceil instead of np.floor to be sure to see all items
+        #     (important for the test set in particular)
+
+    def on_epoch_end(self):
+        """Update indexes after each epoch"""
+        # reinitialise indexes
+        self.indexes = np.arange(len(self.images_paths))
+        # and, if chosen, shuffle them between epochs, to make sure the batches
+        # are not always the same
+        if self.shuffle:
+            np.random.shuffle(self.indexes)
+
+    @staticmethod
+    def padding_value(img):
+        """Compute value to use to pad an image, as the median value of border pixels"""
+        # get height and width of image
+        h, w = img.shape[0:2]
+
+        # concatenate border pixels in an array
+        borders = np.concatenate(
+            (
+                img[:, 0],  # left column
+                img[:, w - 1],  # right column
+                img[0, 1 : w - 2],  # top line without corners
+                img[h - 1, 1 : w - 2],  # bottom line without corners
+            ),
+            axis=0,
+        )
+
+        # compute the median
+        pad_value = np.median(borders)
+
+        return pad_value
+
+    @staticmethod
+    def augmenter(images):
+        """
+        Define a data augmenter which does horizontal flip (50% chance),
+        vertical flip (50% chance), zoom and shear
+        """
+        from imgaug import augmenters as iaa  # image manipulation
+
+        seq = iaa.Sequential(
+            [
+                iaa.Fliplr(0.5),  # horizontally flip 50% of all images
+                iaa.Flipud(0.5),  # vertically flip 50% of all images
+                iaa.Affine(
+                    scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+                    # scale images to 80-120% of their size, individually per axis
+                    # TODO what happens when images are zoomed and there is not enough padding?
+                    shear=(-15, 15),  # shear by -15 to +15 degrees
+                    mode="edge",  # pad images with border picels
+                ),
+            ],
+            random_order=True,  # apply these transformations in random order
+        )
+        return seq(images=images)
+
+    @staticmethod
+    def dbg_save(img, path):
+        for_save = 255 * (img - img.min()) / (img.max() - img.min())
+        for_save = np.array(for_save, np.int)
+        cv.imwrite(path, for_save)
+
+    def __getitem__(self, index):
+        """Generate one batch of data"""
+
+        # pick indexes of images for this batch
+        indexes = self.indexes[index * self.batch_size : (index + 1) * self.batch_size]
+
+        # select and load images from this batch
+        batch_paths = [self.images_paths[i] for i in indexes]
+        # batch_orig_images = [lycon.load(p) / 255 for p in batch_paths]
+        # TODO: No idea why the 255
+        batch_orig_images = []
+        for p in batch_paths:
+            red = cv.imread(p)
+            if red is None:
+                # Image could not be read... TODO: tell somehow
+                continue
+            batch_orig_images.append(red / 255)
+
+        # resize images to the input dimension of the network
+        batch_prepared_images = []
+        input_size = self.input_shape[0]  # NB: assumes square input
+        for img, its_path in zip(batch_orig_images, batch_paths):
+            # self.dbg_save(img, "/tmp/out_raw.jpg")
+            # crop a defined number of pixels from the sides
+            t, r, b, l = self.crop
+            h, w = img.shape[0:2]
+            new_h = h - b
+            new_w = w - r
+            if new_h <= 0 or new_w <= 0:
+                print(sys.stderr, "Crop would make impossible image for ", its_path)
+            else:
+                img = img[t:new_h, l:new_w, :]
+            # TODO crop to object, automatically
+
+            h, w = img.shape[0:2]
+
+            # compute largest dimension (hor or ver)
+            dim_max = max(h, w)
+
+            # upscale small images or downscale large ones
+            if (self.upscale) or (dim_max > input_size):
+                # resize image so that largest dim is now equal to input_size
+                # img = lycon.resize(
+                #     img,
+                #     height=max(h * input_size // dim_max, 1),
+                #     width=max(w * input_size // dim_max, 1),
+                #     interpolation=lycon.Interpolation.AREA
+                # )
+                new_height = max(h * input_size // dim_max, 1)
+                new_width = max(w * input_size // dim_max, 1)
+                img = cv.resize(
+                    img, (new_width, new_height), interpolation=cv.INTER_AREA
+                )
+                h, w = img.shape[0:2]
+
+            # TODO review the following for speed, possibly
+
+            # create a square, empty output, of desired dimension, filled with padding value
+            pad_value = self.padding_value(img)
+            img_square = np.full(self.input_shape, pad_value)
+
+            # compute number of pixels to leave blank
+            offset_ver = int((input_size - h) / 2)  # on top and bottom of image
+            offset_hor = int((input_size - w) / 2)  # on left and right of image
+
+            # replace pixels by input image
+            img_square[offset_ver : offset_ver + h, offset_hor : offset_hor + w] = img
+            batch_prepared_images.append(img_square)
+
+            # self.dbg_save(img_square, "/tmp/out_rsz.jpg")
+
+        # convert to array of _float32_ images, for efficiency
+        batch_prepared_images = np.array(
+            [img for img in batch_prepared_images], dtype="float32"
+        )
+
+        # augment images
+        if self.augment:
+            batch_prepared_images = self.augmenter(batch_prepared_images)
+
+        # extract the labels corresponding to the selected indexes, when they are provided
+        if self.labels is not None:
+            batch_labels = [self.labels[i] for i in indexes]
+            batch_encoded_labels = self.class_encoder.fit_transform(
+                [[l] for l in batch_labels]
+            )
+        else:
+            batch_encoded_labels = None
+
+        # return reshaped images and labels
+        return batch_prepared_images, batch_encoded_labels
