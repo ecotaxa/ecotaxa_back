215c6827273d484b6d4f847e9d3cc003b4635c2e
grololo06 <laurent.salinas@laposte.net>
2026-02-10 08:07:25 +0100

    dev(GPU): a bit more recent sklearn

--- py/ML/Random_forest_classifier.py
+++ py/ML/Random_forest_classifier.py
@@ -11,9 +11,8 @@ from typing import List, Tuple
 import numpy as np  # type: ignore
 
 try:
-    from sklearnex import patch_sklearn  # type: ignore
-
-    patch_sklearn()
+    from sklearnex import patch_sklearn, unpatch_sklearn  # type: ignore
+    from sklearnex.ensemble import RandomForestClassifier as RandomForestClassifierEx
 except ImportError as e:
     pass
 from sklearn.ensemble import RandomForestClassifier  # type: ignore
@@ -33,32 +32,57 @@ class OurRandomForestClassifier(object):
     # CPU resources
     WORKERS = 6
 
+    FULL_INTELEX = False
+
     def __init__(self) -> None:
         # We can build the classifier right away, it's just allocation
-        self.cls = RandomForestClassifier(
-            n_estimators=self.N_ESTIMATORS,
-            min_samples_leaf=self.MIN_SAMPLES_LEAF,
-            n_jobs=self.WORKERS,
-            class_weight=self.CLASS_WEIGHT,
-            verbose=True,
-            random_state=420,
-        )  # TODO: verbose sends logs we can't see :(
+        if self.FULL_INTELEX:
+            self.rfc = RandomForestClassifierEx(
+                n_estimators=self.N_ESTIMATORS,
+                min_samples_leaf=self.MIN_SAMPLES_LEAF,
+                n_jobs=self.WORKERS,
+                class_weight=self.CLASS_WEIGHT,
+                verbose=True,
+                random_state=420,
+            )
+        else:
+            self.rfc = RandomForestClassifier(
+                n_estimators=self.N_ESTIMATORS,
+                min_samples_leaf=self.MIN_SAMPLES_LEAF,
+                n_jobs=self.WORKERS,
+                class_weight=self.CLASS_WEIGHT,
+                verbose=True,
+                random_state=420,
+            )  # TODO: verbose sends logs we can't see :(
 
     def learn_from(self, training_samples: np.ndarray, target_values: np.ndarray):
         """
         Learn the classifier from given data.
         """
-        self.cls.fit(training_samples, target_values)
+        self.rfc.fit(training_samples, target_values)
+
+        if not self.FULL_INTELEX:
+            patch_sklearn()
+            from sklearn.ensemble import RandomForestClassifier as PatchedRF
+
+            cls_ex = PatchedRF()
+            # Manually copy the fitted attributes
+            # This works if the class names match via the patch
+            for attr in self.rfc.__dict__:
+                if attr.endswith("_") or attr.startswith("n_"):
+                    setattr(cls_ex, attr, getattr(self.rfc, attr))
+            unpatch_sklearn()
+            self.rfc = cls_ex
 
     def predict(self, to_predict: np.ndarray) -> Tuple[ClassifIDListT, List[float]]:
         """
         Predict, i.e. return the most likely target value (classif_id) for the given objects.
         Input np array must have the same columns as in training samples during build.
-        For each input line, the returned array contain the best guessed classification ID, and its score.
+        For each input line, the returned array contains the best guessed classification ID, and its score.
         """
-        predict_result = self.cls.predict_proba(to_predict)
+        predict_result = self.rfc.predict_proba(to_predict)
         max_proba = np.argmax(predict_result, axis=1)
-        classif_ids = [int(self.cls.classes_[mc]) for mc in max_proba]
+        classif_ids = [int(self.rfc.classes_[mc]) for mc in max_proba]
         scores = [r[mc] for mc, r in zip(max_proba, predict_result)]
         return classif_ids, scores
 
@@ -70,13 +94,13 @@ class OurRandomForestClassifier(object):
         Input np array must have the same columns as in training samples during build.
         For each input line, the returned array contains all guessed classification ID, and their scores.
         """
-        predict_result = self.cls.predict_proba(to_predict)
+        predict_result = self.rfc.predict_proba(to_predict)
         classif_ids = list()
         scores = list()
         for obj_probas in predict_result:
             # Get the order of predicted probabilities in descending order
             sorted_indexes = np.flip(np.argsort(obj_probas))
             # Add all predictions to result
-            classif_ids.append(self.cls.classes_[sorted_indexes].tolist())
+            classif_ids.append(self.rfc.classes_[sorted_indexes].tolist())
             scores.append(obj_probas[sorted_indexes].tolist())
         return classif_ids, scores
