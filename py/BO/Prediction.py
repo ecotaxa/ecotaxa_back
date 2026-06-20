bd7d0ee4b0634ee22a62096f04ffebed7a2324e1
grololo06 <laurent.salinas@laposte.net>
2026-06-20 10:44:53 +0200

    perfs(ML): slow queries since PG partitioning

--- py/BO/Prediction.py
+++ py/BO/Prediction.py
@@ -50,18 +50,19 @@ class DeepFeatures(object):
         """
         Delete all CNN features from DB, for this project.
         """
-        sub_qry = session.query(ObjectHeader.objid)
-        sub_qry = sub_qry.join(
-            Acquisition, Acquisition.acquisid == ObjectHeader.acquisid
+        qry = session.query(ObjectCNNFeatureVector)
+        qry = qry.join(
+            ObjectHeader, ObjectHeader.objid == ObjectCNNFeatureVector.objcnnid
         )
-        sub_qry = sub_qry.join(
+        qry = qry.where(ObjectHeader.objid.op("<@")(func.obj_in_prj(proj_id)))
+        qry = qry.join(Acquisition, Acquisition.acquisid == ObjectHeader.acquisid)
+        qry = qry.where(Acquisition.acquisid.op("<@")(func.acq_in_prj(proj_id)))
+        qry = qry.join(
             Sample,
             and_(
                 Sample.sampleid == Acquisition.acq_sample_id, Sample.projid == proj_id
             ),
         )
-        qry = session.query(ObjectCNNFeatureVector)
-        qry = qry.filter(ObjectCNNFeatureVector.objcnnid.in_(sub_qry))
         nb_deleted = qry.delete(synchronize_session=False)
         return nb_deleted
 
@@ -77,6 +78,7 @@ class DeepFeatures(object):
         qry = session.query(ObjectHeader.objid, Image.imgid, Image.orig_file_name)
         qry = qry.where(ObjectHeader.objid.op("<@")(func.obj_in_prj(proj_id)))
         qry = qry.join(Acquisition, Acquisition.acquisid == ObjectHeader.acquisid)
+        qry = qry.where(Acquisition.acquisid.op("<@")(func.acq_in_prj(proj_id)))
         qry = qry.join(
             Sample,
             and_(
