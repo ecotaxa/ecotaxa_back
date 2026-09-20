6f3d4e86be430a7ff0f8e8867f29585c0bda6506
grololo06 <laurent.salinas@laposte.net>
2026-09-20 20:04:29 +0200

    test: exercise a (probably legacy) endpoint

--- QA/py/tests/test_taxa_query.py
+++ QA/py/tests/test_taxa_query.py
@@ -3,12 +3,14 @@ from urllib.parse import quote_plus
 
 from starlette import status
 
+from tests.credentials import USER_AUTH
 from tests.test_import import do_test_import
 
 TAXA_SEARCH_URL = "/taxon_set/search?query={query}&project_id={project_id}"
 TAXA_QUERY_URL = "/taxon/{taxon_id}"
 TAXA_SET_QUERY_URL = "/taxon_set/query?ids={taxa_ids}"
 WORMS_TAXA_QUERY_URL = "/worms/{aphia_id}"
+WORMSIFICATION_TAXA_SET_URL = "/taxon_set/wormsification?ids={taxa_ids}"
 ROOT_TAXA_URL = "/taxa"
 
 
@@ -194,41 +196,66 @@ def test_worms_query(fastapi):
     # Unauthenticated call
     rsp = fastapi.get(url)
     assert rsp.json() is None
-    # assert rsp.json() == {
-    #     "children": [],
-    #     "display_name": "Oncaeidae",
-    #     "id": 128586,
-    #     "lineage": [
-    #         "Oncaeidae",
-    #         "Ergasilida",
-    #         "Cyclopoida",
-    #         "Podoplea",
-    #         "Neocopepoda",
-    #         "Copepoda",
-    #         "Hexanauplia",
-    #         "Multicrustacea",
-    #         "Crustacea",
-    #         "Arthropoda",
-    #         "Animalia",
-    #         "Biota",
-    #     ],
-    #     "id_lineage": [
-    #         128586,
-    #         1381349,
-    #         1101,
-    #         155879,
-    #         155876,
-    #         1080,
-    #         889925,
-    #         845959,
-    #         1066,
-    #         1065,
-    #         2,
-    #         1,
-    #     ],
-    #     "name": "Oncaeidae",
-    #     "nb_children_objects": 0,
-    #     "nb_objects": 0,
-    #     "renm_id": None,
-    #     "type": "P",
-    # }
+
+
+def test_wormsification_taxa_set(fastapi):
+    # 1. Unauthenticated call with multiple IDs (taxon 1 - Biota, taxon 233 - Cyanobacteria)
+    url = WORMSIFICATION_TAXA_SET_URL.format(taxa_ids="1:233")
+    rsp = fastapi.get(url)
+    assert rsp.status_code == status.HTTP_200_OK
+    data = rsp.json()
+    assert "1" in data
+    assert "233" in data
+
+    # Check Biota (Taxon 1)
+    biota = data["1"]
+    assert biota["id"] == 1
+    assert biota["name"] == "Biota"
+    assert biota["aphia_id"] == 1
+    assert biota["display_name"] == "Biota"
+    assert biota["type"] == "P"
+    assert biota["status"] == "A"
+    assert biota["rank"] == "Superdomain"
+    assert biota["lineage"] == ["Biota"]
+    assert biota["id_lineage"] == [1]
+
+    # Check Cyanobacteria (Taxon 233)
+    cyano = data["233"]
+    assert cyano["id"] == 233
+    assert cyano["name"] == "Cyanobacteria"
+    assert cyano["aphia_id"] == 146537
+    assert cyano["display_name"] == "Cyanobacteria<Negibacteria"
+    assert cyano["type"] == "P"
+    assert cyano["status"] == "A"
+    assert cyano["lineage"] == ["Cyanobacteria", "Negibacteria", "Bacteria", "Biota"]
+    assert cyano["id_lineage"] == [233, 100089, 3, 1]
+
+    # 2. Taxon 849 is a renamed phylo taxon (renm_id=233)
+    url = WORMSIFICATION_TAXA_SET_URL.format(taxa_ids="849")
+    rsp = fastapi.get(url)
+    assert rsp.status_code == status.HTTP_200_OK
+    data = rsp.json()
+    assert "233" in data
+    assert data["233"]["aphia_id"] == 146537
+
+    # 3. Test various delimiters (comma, pipe)
+    url_comma = WORMSIFICATION_TAXA_SET_URL.format(taxa_ids="1,233")
+    rsp_comma = fastapi.get(url_comma)
+    assert rsp_comma.status_code == status.HTTP_200_OK
+    assert set(rsp_comma.json().keys()) == {"1", "233"}
+
+    url_pipe = WORMSIFICATION_TAXA_SET_URL.format(taxa_ids="1|233")
+    rsp_pipe = fastapi.get(url_pipe)
+    assert rsp_pipe.status_code == status.HTTP_200_OK
+    assert set(rsp_pipe.json().keys()) == {"1", "233"}
+
+    # 4. Non-existent taxon ID
+    url_nonexistent = WORMSIFICATION_TAXA_SET_URL.format(taxa_ids="99999999")
+    rsp_nonexistent = fastapi.get(url_nonexistent)
+    assert rsp_nonexistent.status_code == status.HTTP_200_OK
+    assert rsp_nonexistent.json() == {}
+
+    # 5. Authenticated call
+    rsp_auth = fastapi.get(url, headers=USER_AUTH)
+    assert rsp_auth.status_code == status.HTTP_200_OK
+    assert rsp_auth.json() == data
